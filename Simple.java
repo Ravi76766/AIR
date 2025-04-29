2c7a501d775f33b0c34da3d9f5614f628d72f0d4
Ravi76766 <raviu2687@gmail.com>
2025-04-29 11:35:32 +0530

    save

--- Simple.java
+++ Simple.java
@@ -9,7 +9,6 @@ public class Simple {
         int difference = num1 - num2;
         int product = num1 * num2;
         int quotient = num1 / num2;
-        int remainder = num1 % num2;
 
         // Displaying the results
         System.out.println("Number 1: " + num1);
@@ -18,6 +17,5 @@ public class Simple {
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
-        System.out.println("Remainder: " + remainder);
     }
 }
